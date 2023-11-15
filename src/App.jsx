ccd273ca9a8c72053c036291456c0059a977eb4b
youssefhammani <youssefhammani04@gmail.com>
2023-11-15 09:19:19 +0100

    Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import VerifyEmail from './pages/VerifyEmail'
 import { lazy, Suspense } from 'react';
 import ForgotPassword from './pages/ForgotPassword';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {QueryClient,QueryClientProvider} from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import useAuth from './hooks/useAuth';
-import { PrivateRoute } from './routes/privateRoute';
+
+import { Dashboard } from "@/layouts";
+
+// import { PrivateRoute } from './routes/privateRoute';
+import { PrivateRoute } from './routes/PrivateRoute';
 const Home = lazy(() => import('./pages/Home'))
 const Inscreption = lazy(() => import('./pages/Inscreption'))
 import Redirect from './components/Redirect';
@@ -26,37 +30,40 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-      <Suspense fallback={
-        <div className='w-screen h-screen flex justify-center items-center'>
-          <div className='w-10 h-10 border-4 border-red-500 rounded-full animate-spin'></div>
-        </div>
-      }>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route 
-            path='/inscription' 
-            element={
-              isAuthenticated 
-                ? <Redirect />
-                : <Inscreption />
+        <Suspense fallback={
+          <div className='w-screen h-screen flex justify-center items-center'>
+            <div className='w-10 h-10 border-4 border-red-500 rounded-full animate-spin'></div>
+          </div>
+        }>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route
+              path='/inscription'
+              element={
+                isAuthenticated
+                  ? <Redirect />
+                  : <Inscreption />
               }
-          />
-          <Route 
-            path='/profile' 
-            element={
-              <PrivateRoute 
-                authenticated={isAuthenticated}
-                element={<Profile/>}
-              />
-            }
-          />
-          <Route path='/verifyEmail/:token?' element={<VerifyEmail />} />
-          <Route path='/forgotPassword' element={<ForgotPassword/>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+            />
+            <Route
+              path='/profile'
+              element={
+                <PrivateRoute
+                  authenticated={isAuthenticated}
+                  element={<Profile />}
+                />
+              }
+            />
+            <Route path='/verifyEmail/:token?' element={<VerifyEmail />} />
+            <Route path='/forgotPassword' element={<ForgotPassword />} />
+            <Route path="*" element={<NotFound />} />
+            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+            <Route path="/dashboard/*" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+          </Routes>
+        </Suspense>
       </Router>
-      <ReactQueryDevtools/>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
