d3301936505dbb7b917ae535d000273e4573a75f
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-11-16 09:00:54 +0100

    realiser map manager pour ajouter un restaurant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,64 +1,67 @@
-import './App.css'
-import VerifyEmail from './pages/VerifyEmail'
-import { lazy, Suspense } from 'react';
-import ForgotPassword from './pages/ForgotPassword';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {QueryClient,QueryClientProvider} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import NotFound from './pages/NotFound';
-import Profile from './pages/Profile';
-import useAuth from './hooks/useAuth';
-import { PrivateRoute } from './routes/privateRoute';
-const Home = lazy(() => import('./pages/Home'))
-const Inscreption = lazy(() => import('./pages/Inscreption'))
-import Redirect from './components/Redirect';
+import "./App.css";
+import VerifyEmail from "./pages/VerifyEmail";
+import { lazy, Suspense } from "react";
+import ForgotPassword from "./pages/ForgotPassword";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import useAuth from "./hooks/useAuth";
+import { PrivateRoute } from "./routes/privateRoute";
+const Home = lazy(() => import("./pages/Home"));
+const Inscreption = lazy(() => import("./pages/Inscreption"));
+import Redirect from "./components/Redirect";
+import Container from "./components/Maps/mapFilter/MapContainer";
+import MapManager from "./components/Maps/mapManager.jsx";
+
 function App() {
   const { isLoading, isAuthenticated } = useAuth();
   if (isLoading) {
     return (
-      <div className='w-screen h-screen flex justify-center items-center'>
-        <div className='w-10 h-10 border-4 border-red-500 rounded-full animate-spin'></div>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-10 h-10 border-4 border-red-500 rounded-full animate-spin"></div>
       </div>
     );
   }
 
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-      <Suspense fallback={
-        <div className='w-screen h-screen flex justify-center items-center'>
-          <div className='w-10 h-10 border-4 border-red-500 rounded-full animate-spin'></div>
-        </div>
-      }>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route 
-            path='/inscription' 
-            element={
-              isAuthenticated 
-                ? <Redirect />
-                : <Inscreption />
+        <Suspense
+          fallback={
+            <div className="w-screen h-screen flex justify-center items-center">
+              <div className="w-10 h-10 border-4 border-red-500 rounded-full animate-spin"></div>
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/inscription"
+              element={isAuthenticated ? <Redirect /> : <Inscreption />}
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute
+                  authenticated={isAuthenticated}
+                  element={<Profile />}
+                />
               }
-          />
-          <Route 
-            path='/profile' 
-            element={
-              <PrivateRoute 
-                authenticated={isAuthenticated}
-                element={<Profile/>}
-              />
-            }
-          />
-          <Route path='/verifyEmail/:token?' element={<VerifyEmail />} />
-          <Route path='/forgotPassword' element={<ForgotPassword/>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+            />
+            <Route path="/verifyEmail/:token?" element={<VerifyEmail />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/maps" element={<Container />} />
+            <Route path="/manager/maps" element={<MapManager />} />
+          </Routes>
+        </Suspense>
       </Router>
-      <ReactQueryDevtools/>
+      <ReactQueryDevtools />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
