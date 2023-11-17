1952b23d4ccfe2964e037f8f2e8b44785ed30bce
faissal17 <faissalaoukacha@gmail.com>
2023-11-17 08:45:23 +0100

    fix the toutes problemes

--- backend/src/controllers/restaurant/addRestaurant.js
+++ backend/src/controllers/restaurant/addRestaurant.js
@@ -2,9 +2,10 @@ const express = require("express");
 const Restaurant = require("../../frameworks/database/mongo/schemas/restaurants.schema");
 
 const addRestaurent = async (req, res) => {
-  const { name, slug } = req.body;
-  if ((!name, !slug)) {
-    res.status(404).json({ message: "All field are required" });
+  const { user, name, slug, picture, status, tags, brands, categories } =
+    req.body;
+  if (!user || !name || !slug) {
+    res.status(400).json({ message: "All field are required" });
   }
 };
 
