806d97836848d619cccc775412d0b669874f7464
mustaphabousildev <bousilmustapha@gmail.com>
2023-11-22 13:20:28 +0100

    add order and orderDetails schema

--- backend/src/frameworks/database/mongo/schemas/orders-details.schema.js
+++ backend/src/frameworks/database/mongo/schemas/orders-details.schema.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 
 const orderDetailsSchema = new mongoose.Schema({
   // Other tag-related fields
+  order:{
+    type:String,
+    ref:'Order',
+    default:null
+  },
+  item:{
+    type:String,
+    ref:'Item',
+    default:null
+  },
+  quantity:{
+    type:Number,
+    default:0
+  },
+  deletedAt:{
+    type:Date,
+    default:null
+  }
 });
 
-const OrderDetails = mongoose.model('OrderDetails', orderDetailsSchema);
-
-module.exports = OrderDetails;
+module.exports = orderDetailsSchema;
