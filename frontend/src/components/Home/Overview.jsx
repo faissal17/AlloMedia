e689c5c19b5d639f89286fbe1512eccee7a38031
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-11-25 20:21:39 +0100

    fd

--- frontend/src/components/Home/Overview.jsx
+++ frontend/src/components/Home/Overview.jsx
@@ -101,22 +101,30 @@ const Overview = () => {
           />
           <div>
             {shwoRestaurant && dataRestaurant && (
-              <li className=" absolute top-[100%] left-0 w-full bg-white rounded-md shadow-md">
-                <ul className=" flex flex-col gap-2">
-                  {dataRestaurant.map((data, index) => (
-                    <li
-                      key={data.slug}
-                      className=" border-b border-gray-300 py-2 px-3"
-                    >
-                      <Link to={`/restaurant/?slug=${data.slug}`}>
-                        <h3 className=" text-lg font-semibold text-gray-800">
-                          {data.name}
-                        </h3>
-                      </Link>
+              <div className=" absolute top-[100%] left-0 w-full bg-white rounded-md shadow-md">
+                <ul className=" flex flex-col gap-2 ">
+                  {dataRestaurant && dataRestaurant.length > 0 ? (
+                    dataRestaurant.map((data, index) => (
+                      <li
+                        key={data.slug}
+                        className="border-b border-gray-300 py-2 px-3"
+                      >
+                        <Link to={`/restaurant/?slug=${data.slug}`}>
+                          <h3 className="text-lg font-semibold text-gray-800">
+                            {data.name}
+                          </h3>
+                        </Link>
+                      </li>
+                    ))
+                  ) : (
+                    <li className="border-b border-gray-300 py-2 px-3">
+                      <h3 className="text-lg font-semibold text-gray-800">
+                        No result
+                      </h3>
                     </li>
-                  ))}
+                  )}
                 </ul>
-              </li>
+              </div>
             )}
           </div>
         </div>
