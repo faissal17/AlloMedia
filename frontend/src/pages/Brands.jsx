8487f03c4eb763fb13ff42f8bf9a87ee96af88cc
mustaphabousildev <bousilmustapha@gmail.com>
2023-11-29 09:57:57 +0100

    brand dash

--- frontend/src/pages/Brands.jsx
+++ frontend/src/pages/Brands.jsx
@@ -1,10 +1,92 @@
 
-import React from "react"
+import React,{ useEffect, useState } from "react"
 import pizza from "../assets/pizza.jpg";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import {
+  useAddBrandMutation,
+  useGetBrandQuery,
+  useDeleteBrandMutation,
+  useUpdateBrandMutation,
+} from "../redux/service/brands/brandApi";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const Brands = () => {
+  const [open, setOpen] = React.useState(false);
+  const [valueModel, setModel] = useState(false);
+  const [valueCategory, setCateory] = useState({
+    id: "",
+    name: "",
+  });
+  const {
+    data: brands,
+    error: errorBrands,
+    isLoading: isLoadingBrands,
+    refetch,
+  } = useGetBrandQuery();
+
+  const [addCategory, { data, error, isLoading }] = useAddBrandMutation();
+  const [deleteCategory, { data: dataDelete }] = useDeleteBrandMutation();
+  const [updateCategory, { data: dataUpdate }] = useUpdateBrandMutation();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+    setModel(false);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleEdit = async (e) => {
+    await setCateory({
+      id: e.target.id,
+    });
+    setOpen(true);
+    setModel(true);
+  };
+
+  const handleChange = (e) => {
+    setCateory({ ...valueCategory, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    await addCategory(valueCategory);
+    setOpen(false);
+    refetch();
+  };
+
+  const handleDelete = async (e) => {
+    await deleteCategory({
+      id: e.target.id,
+    });
+    refetch();
+  };
+
+  const handleSubmitEdit = async (e) => {
+    await updateCategory({ ...valueCategory });
+    setOpen(false);
+    refetch();
+  };
   return (
     <React.Fragment>
-      <table className="divide-gray-200 w-full mt-5">
+      <div className="flex justify-end p-8">
+        <div className="flex">
+          <button
+            onClick={handleClickOpen}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-5"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <table className="divide-gray-200 w-full mt-5 ">
         <thead className="bg-gray-200">
           <tr>
             <th
@@ -17,13 +99,13 @@ const Brands = () => {
               scope="col"
               className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
             >
-              Picture
+              picture
             </th>
             <th
               scope="col"
               className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
             >
-              Status
+              status
             </th>
             <th
               scope="col"
@@ -37,7 +119,6 @@ const Brands = () => {
             >
               date
             </th>
-            
             <th
               scope="col"
               className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -47,49 +128,126 @@ const Brands = () => {
           </tr>
         </thead>
         <tbody className="bg-gray-100 divide-y divide-gray-200">
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 h-10 w-10">
-                  <img className="h-10 w-10 rounded-full" src={pizza} />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    MonteCarlo
+          {brands &&
+            brands.content?.map((brand) => (
+              <tr key={brand.id}>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="ml-4">
+                      <div className="text-sm font-medium text-gray-900">
+                        {brand.name}
+                      </div>
+                      <div className="text-sm text-gray-500">
+                        {brand.description}
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="text-sm text-gray-900">
-                Regional Paradigm Technician
-              </div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Excellent
-              </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              Life
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              new 200
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              italiane
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-              <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                Edit
-              </a>
-              <a href="#" className="ml-2 text-red-600 hover:text-red-900">
-                Delete
-              </a>
-            </td>
-          </tr>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex-shrink-0 w-10 h-10">
+                    <img
+                      className="w-full h-full rounded-full"
+                      src={brand.picture}
+                      alt=""
+                    />
+                  </div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    {brand.status === 1 ? "Active" : "Inactive"}
+                  </span>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {brand.user.first_name + " " + brand.user.last_name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {new Date(brand.createdAt).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <button
+                    id={brand._id}
+                    onClick={handleEdit}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    id={brand._id}
+                    onClick={handleDelete}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth={true}
+        maxWidth={"sm"}
+      >
+        <DialogTitle>{"Create Category"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-slide-description"
+            className="flex justify-center w-full"
+          >
+            <div className="flex flex-col md:flex-row justify-center pt-8">
+              <div className="w-full">
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    for="name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="name"
+                  />
+                </div>
+              </div>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          {!valueModel && (
+            <Button
+              color="primary"
+              variant="contained"
+              autoFocus
+              onClick={handleSubmit}
+            >
+              Create
+            </Button>
+          )}
+
+          {valueModel && (
+            <Button
+              color="primary"
+              variant="contained"
+              autoFocus
+              onClick={handleSubmitEdit}
+            >
+              Edit
+            </Button>
+          )}
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   )
 }
--- frontend/src/redux/service/brands/brandApi.js
+++ frontend/src/redux/service/brands/brandApi.js
@@ -2,13 +2,44 @@ import { ApiSlice } from "../../api/apiSlice";
 
 export const brandsApi = ApiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getAllBrands: builder.query({
+    getBrand: builder.query({
       query: () => ({
         url: "brands",
         method: "GET",
       }),
     }),
+    addBrand: builder.mutation({
+      query: (body) => ({
+        url: "brands",
+        method: "POST",
+        body,
+      }),
+    }),
+    deleteBrand: builder.mutation({
+      query: (body) => ({
+        url: `brands/`,
+        method: "DELETE",
+        body: body,
+      }),
+    }),
+    updateBrand: builder.mutation(
+      {
+        query: (body) => ({
+          url: `brands`,
+          method: "PATCH",
+          body: body,
+        }),
+      }
+      // {
+      //   dispatchConditionRejection: false,
+      // }
+    ),
   }),
 });
 
-export const { useGetAllBrandsQuery } = brandsApi;
+export const {
+  useGetBrandQuery,
+  useAddBrandMutation,
+  useDeleteBrandMutation,
+  useUpdateBrandMutation,
+} = brandsApi;
