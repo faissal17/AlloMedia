cab977bfbeceedcf11f03fefc9edec0677b6b4ae
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-12-01 08:53:18 +0100

    role all users

---
+++ frontend/src/components/Maps/tracking/user.jsx
@@ -0,0 +1,23 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+const TrackingLivreur = () => {
+  return (
+    <div>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default TrackingLivreur;
