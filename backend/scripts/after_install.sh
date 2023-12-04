64fcb4f673e6e7d8035f98200b6604a461b8c8b1
mustaphabousildev <bousilmustapha@gmail.com>
2023-12-04 20:40:08 +0100

    fllg

--- backend/scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /AlloMedia/deploy.log
-
-echo 'cd /AlloMedia' >> /AlloMedia/deploy.log
-cd /AlloMedia >> /AlloMedia/deploy.log
-
-echo 'npm install' >> /AlloMedia/deploy.log 
-npm install >> /AlloMedia/deploy.log
--- backend/scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /AlloMedia/deploy.log
-
-echo 'pm2 restart nodejs-express-app' >> /AlloMedia/deploy.log
-pm2 restart nodejs-express-app >> /AlloMedia/deploy.log
