6df75586472bfce4fa2eef6011e0694737ddf6d0
faissal17 <faissalaoukacha@gmail.com>
2023-12-04 15:00:09 +0100

    remove redis

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,35 +6,20 @@ const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3000;
 const routes = require("./frameworks/expressSpecific/routes");
 const mongoose = require("./frameworks/database/mongo/index");
-const http=require('http')
+const http = require("http");
 const { Server } = require("socket.io");
 
 const axios = require("axios");
 const cors = require("cors");
-const redis = require("redis");
-
 
 // added Fake API
 const MOCK_API = "https://jsonplaceholder.typicode.com/users/";
-
-
-
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 //this is for form data  urlencoded is meaning of form data extended is false means only string and array
 app.use(bodyParser.urlencoded({ extended: false }));
 const { connect: connectMongo } = require("./frameworks/database/mongo");
-
-
-
-const redisClient = redis.createClient({
-  legacyMode: true,
-  PORT: 6379,
-});
-
-
-redisClient.connect().catch(console.error);
 
 const allowedOrigins = ["http://localhost:5173"];
 const corsOptions = {
@@ -64,16 +49,14 @@ module.exports = {
     const addNewUser = (username, socketId) => {
       !onlineUsers.some((user) => user.username === username) &&
         onlineUsers.push({ username, socketId });
-        console.log('from add function user 0')
-        console.log(onlineUsers[0].socketId)
-        console.log('all users')
-        console.log(onlineUsers)
-        
+      console.log("from add function user 0");
+      console.log(onlineUsers[0].socketId);
+      console.log("all users");
+      console.log(onlineUsers);
     };
     const getUser = (username) => {
       return onlineUsers.find((user) => user.username === username);
     };
-
 
     const server = http.createServer(app);
     const io = new Server(server, {
@@ -83,7 +66,7 @@ module.exports = {
       },
     });
     let d = 0;
-    
+
     io.on("connection", (socket) => {
       console.log(` this is the fucking id :${socket.id}`);
 
@@ -96,30 +79,19 @@ module.exports = {
       //   console.log(`User with his this mae is: ${socket.id} is create account with this name ${data} `)
       // })
 
-      socket.on('sendNotification',(data)=>{
-          console.log(data)
-          io.emit("getNotification", {
-            d:'you except notification'
-      });
-
-      
-        
-        
-        
-
-        
+      socket.on("sendNotification", (data) => {
+        console.log(data);
+        io.emit("getNotification", {
+          d: "you except notification",
+        });
       });
       socket.on("sendNotificationJob", (data) => {
         console.log("its comming");
         console.log(data);
         io.emit("getNotificationJob", {
-          data:data
+          data: data,
+        });
       });
-      })
-      
-     
-      
-
 
       socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
@@ -140,27 +112,7 @@ module.exports = {
         res.status(500).send(err);
       }
     });
-    app.get("/cache/user/:email", async (req, res) => {
-      const email = req.params.email;
 
-      try {
-        redisClient.get(email, async (err, response) => {
-          console.log(response);
-          if (response) {
-            console.log("User successfully retrieved from cache");
-            res.status(200).send(JSON.parse(response));
-          } else {
-            const response = await axios.get(`${MOCK_API}?email=${email}`);
-            const user = response.data;
-            redisClient.setex(email, 600, JSON.stringify(user));
-            console.log("User successfully retrieved from the API");
-            res.status(200).send(user);
-          }
-        });
-      } catch (err) {
-        res.status(500).send({ error: err.message });
-      }
-    });
     // socket.on("disconnect", () => {
     //   console.log("User Disconnected", socket.id);
     // });
--- backend/src/frameworks/repositories/mongo/products.repository.js
+++ backend/src/frameworks/repositories/mongo/products.repository.js
@@ -1,147 +1,101 @@
-const mongoose=require('mongoose')
-const redis = require('redis');
-const { createClient} =require('redis')
-const entityName="Items"
-const redisClient = redis.createClient({
-    legacyMode: true,
-    PORT: 6379
-  })
-  redisClient.connect().catch(console.error)
+const mongoose = require("mongoose");
+
+const entityName = "Items";
 
 const {
-    schemas:{
-        item:itemSchema
-    }
-}=require('../../database/mongo') 
-let globalItems=[]
-const repository=()=>{
-    //schema
-    const Item=mongoose.model(entityName,itemSchema)
-    
-    return {
-        add:async item=>{
-            console.log('I am here')
-            const mongoObject = new Item(item);
-            let itemss=await mongoObject.save();
-            const itemAll = await Item.find()
-            .populate('user','first_name last_name username email mobile picture role')
-            .populate('brand','name picture')
-            .populate('category','name picture')
-            .populate('menu','name')
-            .populate('tags','name');
-            globalItems=itemAll
-            // Save to Redis
-            redisClient.get('items', (err, cachedItems) => {
-                if (err) {
-                    console.error('Error getting items from Redis:', err);
-                    return;
-                }
-                let items = [];
-                if (cachedItems) {
-                    // items = JSON.parse(cachedItems);
-                    // items.push(itemss);
-                    // globalItems=items
-                    redisClient.setEx('items', 3600, JSON.stringify(globalItems));
-                    console.log('cache exist')
-                    redisClient.setEx('items', 3600, JSON.stringify(globalItems));
-                }else{
-                    redisClient.setEx('items', 3600, JSON.stringify(globalItems));
-                }
-                
-                // Set the updated items array back to Redis
-                
-                
-            });
-            return itemss
-        },
-        update:async item=>{
-            const { id , updates }=item
-            delete item.id 
-            const updatedItem=await Item.findByIdAndUpdate(id,{
-                ...updates,
-                updatedAt:new Date()
-            },{
-                new:true 
-            })
-            // Update Redis cache
-            const itemAll = await Item.find()
-            .populate('user','first_name last_name username email mobile picture role')
-            .populate('brand','name picture')
-            .populate('category','name picture')
-            .populate('menu','name')
-            .populate('tags','name');
-            redisClient.get('items', (err, cachedItems) => {
-                if (err) {
-                    console.error('Error getting items from Redis:', err);
-                    return;
-                }
-                let items = [];
-                if (cachedItems) {
-                    // items = JSON.parse(cachedItems);
-                    // items.push(itemss);
-                    // globalItems=items
-                    redisClient.setEx('items', 3600, JSON.stringify(itemAll));
-                    console.log('cache exist')
-                    redisClient.setEx('items', 3600, JSON.stringify(itemAll));
-                }else{
-                    redisClient.setEx('items', 3600, JSON.stringify(itemAll));
-                }
-            });
-            return updatedItem
-        },
-        delete:async item=>{
-            const { id }=item
-            delete item.id 
-            const deletedItem = await Item.findByIdAndDelete(id);
-             // Update Redis cache
-            redisClient.get('items', (err, cachedItems) => {
-                if (err) {
-                    console.error('Error getting items from Redis:', err);
-                    return;
-                }
-                let items = [];
-                if (cachedItems) {
-                    items = JSON.parse(cachedItems);
-                    const index = items.findIndex(i => i._id === id);
-                    if (index !== -1) {
-                        items.splice(index, 1);
-                        redisClient.setEx('items', 3600, JSON.stringify(items));
-                    }
-                }
-            });
-            return deletedItem
-        },
-        getById:async id=>{
-            const item=await Item.findOne({
-                _id:id,
-                deletedAt:{
-                    $exists:false
-                }
-            })
-            if (!item) {
-                throw new Error(`Brand with ID ${id} does not exist or has been deleted.`);
-            }
-            return item;
-        },
-        getAll: async () => {
-            const items = await Item.find()
-                .populate('user','first_name last_name username email mobile picture role')
-                .populate('brand','name picture')
-                .populate('category','name picture')
-                .populate('menu','name')
-                .populate('tags','name')
-                ;
-            //console.log(items)
-            globalItems=items
-            //console.log(globalItems)
-            redisClient.setEx('items', 3600, JSON.stringify(globalItems));
-            if (!items) {
-            throw new Error(`categories does not exist or has been deleted.`);
-            }
-            return items;
-                    
-        },
-    }
-}
+  schemas: { item: itemSchema },
+} = require("../../database/mongo");
+let globalItems = [];
+const repository = () => {
+  //schema
+  const Item = mongoose.model(entityName, itemSchema);
 
-module.exports=repository()
+  return {
+    add: async (item) => {
+      console.log("I am here");
+      const mongoObject = new Item(item);
+      let itemss = await mongoObject.save();
+      const itemAll = await Item.find()
+        .populate(
+          "user",
+          "first_name last_name username email mobile picture role"
+        )
+        .populate("brand", "name picture")
+        .populate("category", "name picture")
+        .populate("menu", "name")
+        .populate("tags", "name");
+      globalItems = itemAll;
+      // Save to Redis
+
+      return itemss;
+    },
+    update: async (item) => {
+      const { id, updates } = item;
+      delete item.id;
+      const updatedItem = await Item.findByIdAndUpdate(
+        id,
+        {
+          ...updates,
+          updatedAt: new Date(),
+        },
+        {
+          new: true,
+        }
+      );
+      // Update Redis cache
+      const itemAll = await Item.find()
+        .populate(
+          "user",
+          "first_name last_name username email mobile picture role"
+        )
+        .populate("brand", "name picture")
+        .populate("category", "name picture")
+        .populate("menu", "name")
+        .populate("tags", "name");
+
+      return updatedItem;
+    },
+    delete: async (item) => {
+      const { id } = item;
+      delete item.id;
+      const deletedItem = await Item.findByIdAndDelete(id);
+      // Update Redis cache
+
+      return deletedItem;
+    },
+    getById: async (id) => {
+      const item = await Item.findOne({
+        _id: id,
+        deletedAt: {
+          $exists: false,
+        },
+      });
+      if (!item) {
+        throw new Error(
+          `Brand with ID ${id} does not exist or has been deleted.`
+        );
+      }
+      return item;
+    },
+    getAll: async () => {
+      const items = await Item.find()
+        .populate(
+          "user",
+          "first_name last_name username email mobile picture role"
+        )
+        .populate("brand", "name picture")
+        .populate("category", "name picture")
+        .populate("menu", "name")
+        .populate("tags", "name");
+      //console.log(items)
+      globalItems = items;
+      //console.log(globalItems)
+      if (!items) {
+        throw new Error(`categories does not exist or has been deleted.`);
+      }
+      return items;
+    },
+  };
+};
+
+module.exports = repository();
