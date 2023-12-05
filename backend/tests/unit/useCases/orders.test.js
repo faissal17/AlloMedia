75cbd6d02d324118618c7aa953db9c668c0aa3f1
faissal17 <faissalaoukacha@gmail.com>
2023-12-05 09:36:31 +0100

    start working on delete function test

--- backend/tests/unit/useCases/orders.test.js
+++ backend/tests/unit/useCases/orders.test.js
@@ -4,7 +4,7 @@ const chance = new Chance();
 const { v4: uuidv4 } = require("uuid");
 
 const {
-  order: { addOrderUseCase, getAllOrderUseCase },
+  order: { addOrderUseCase, getAllOrderUseCase, deleteOrderUseCase },
 } = require("../../../src/useCases");
 
 jest.mock(".././../../src/frameworks/repositories");
@@ -50,7 +50,7 @@ describe("Order use cases", () => {
 
   const dependencies = { ordersRepository: mockOrderRepo };
 
-  describe("add order use case", () => {
+  describe("order use case", () => {
     test("new order should be added", async () => {
       const fakeId = uuidv4();
       const mockOrderData = {
@@ -94,51 +94,55 @@ describe("Order use cases", () => {
         comment: "DONE",
       });
     }, 20000);
-  });
 
-  // test get All orders from database
+    // test get All orders from database
 
-  test("get all orders", async () => {
-    const fakeId = uuidv4();
-    const mockOrderData = {
-      id: fakeId,
-      subTotal: chance.integer({ min: 50, max: 200 }),
-      discount: chance.integer(),
-      tax: chance.integer(),
-      total: chance.integer(),
-      firstName: chance.first(),
-      lastName: chance.last(),
-      email: chance.email(),
-      phone: chance.phone(),
-      lineOne: chance.address(),
-      lineTwo: chance.address(),
-      city: chance.city(),
-      zipCode: chance.zip(),
-      status: {
+    test("get all orders", async () => {
+      const fakeId = uuidv4();
+      const mockOrderData = {
+        id: fakeId,
+        subTotal: chance.integer({ min: 50, max: 200 }),
+        discount: chance.integer(),
+        tax: chance.integer(),
+        total: chance.integer(),
+        firstName: chance.first(),
+        lastName: chance.last(),
+        email: chance.email(),
+        phone: chance.phone(),
+        lineOne: chance.address(),
+        lineTwo: chance.address(),
+        city: chance.city(),
+        zipCode: chance.zip(),
+        status: {
+          comment: "DONE",
+        },
+      };
+
+      mockOrderRepo.addOrder.mockResolvedValue(mockOrderData);
+
+      const useCaseInstance = getAllOrderUseCase(dependencies);
+      const getOrder = await useCaseInstance.execute(mockOrderData);
+
+      expect(getOrder.id).toBeDefined();
+      expect(getOrder.subTotal).toEqual(mockOrderData.subTotal);
+      expect(getOrder.discount).toEqual(mockOrderData.discount);
+      expect(getOrder.tax).toEqual(mockOrderData.tax);
+      expect(getOrder.total).toEqual(mockOrderData.total);
+      expect(getOrder.firstName).toEqual(mockOrderData.firstName);
+      expect(getOrder.lastName).toEqual(mockOrderData.lastName);
+      expect(getOrder.email).toEqual(mockOrderData.email);
+      expect(getOrder.phone).toEqual(mockOrderData.phone);
+      expect(getOrder.lineOne).toEqual(mockOrderData.lineOne);
+      expect(getOrder.lineTwo).toEqual(mockOrderData.lineTwo);
+      expect(getOrder.city).toEqual(mockOrderData.city);
+      expect(getOrder.zipCode).toEqual(mockOrderData.zipCode);
+      expect(getOrder.status).toEqual({
         comment: "DONE",
-      },
-    };
+      });
+    }, 20000);
 
-    mockOrderRepo.addOrder.mockResolvedValue(mockOrderData);
+    // test the Delete function
 
-    const useCaseInstance = getAllOrderUseCase(dependencies);
-    const getOrder = await useCaseInstance.execute(mockOrderData);
-
-    expect(getOrder.id).toBeDefined();
-    expect(getOrder.subTotal).toEqual(mockOrderData.subTotal);
-    expect(getOrder.discount).toEqual(mockOrderData.discount);
-    expect(getOrder.tax).toEqual(mockOrderData.tax);
-    expect(getOrder.total).toEqual(mockOrderData.total);
-    expect(getOrder.firstName).toEqual(mockOrderData.firstName);
-    expect(getOrder.lastName).toEqual(mockOrderData.lastName);
-    expect(getOrder.email).toEqual(mockOrderData.email);
-    expect(getOrder.phone).toEqual(mockOrderData.phone);
-    expect(getOrder.lineOne).toEqual(mockOrderData.lineOne);
-    expect(getOrder.lineTwo).toEqual(mockOrderData.lineTwo);
-    expect(getOrder.city).toEqual(mockOrderData.city);
-    expect(getOrder.zipCode).toEqual(mockOrderData.zipCode);
-    expect(getOrder.status).toEqual({
-      comment: "DONE",
-    });
-  }, 20000);
+    test("delete product by useCase", async () => {});
+  });
 });
